ccf3f45117482f74991474530150ebae0ee9468a
jrabadan <corporativoemeth@gmail.com>
2021-10-09 12:01:04 -0500

    se quita el puerto de los microservicios

--- gatling/src/test/scala/computerdatabase/BasicSimulation.scala
+++ gatling/src/test/scala/computerdatabase/BasicSimulation.scala
@@ -7,22 +7,23 @@ import scala.concurrent.duration._
 class BasicSimulation extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("http://192.168.0.6:8090/microservicio") // Here is the root for all relative URLs
+    .baseUrl("http://localhost:8085/apimicros/microservicio") // Here is the root for all relative URLs
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("/users"))
+  val scn = scenario("Consulta usuarios de negocio") // A scenario is a chain of requests and pauses
+    .exec(http("Consulta Usuarios")
+      .get("/users"))
     .pause(7)
-    .exec(http("Validar ping"))
-      .get("/ping")
+    .exec(http("Validar ping")
+      .get("/ping"))
     .pause(7) // Note that Gatling has recorded real time pauses
      
 
   setUp(
     scn.inject(
-      rampUsers(10).during(5.seconds),
+      rampUsers(100).during(5.seconds),
   ).protocols(httpProtocol))
 }
